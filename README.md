d552a5d3ae3aec74c953afca1bac33532809437c
biud436 <biud436@gmail.com>
2021-12-24 16:21:14 +0900

    added webpack build

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 # Introduction
+
 This plugin package includes helpful features to get you on your way to create your game in RPG Maker MZ.
 
+# How to build
+
+To build this plugin, you need to run the following command:
+
+```
+yarn install
+yarn run build
+```
+
 # Credit and Thanks
+
 Biud436.
 
 # Terms of Use
+
 Free for commercial and non-commercial use.
 
 # License
+
 The MIT License (MIT)
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const fs = require("fs");
+const webpack = require("webpack");
+
+const target = {
+  mode: "development", // none' | 'development' | 'production'
+  entry: `./RS_MessageSystem.ts`,
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: `RS_MessageSystem.js`,
+  },
+  target: "web",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+        include: [path.resolve(__dirname)],
+      },
+    ],
+  },
+  plugins: [
+    {
+      apply: (compiler) => {
+        compiler.hooks.afterEmit.tap("AfterEmitPlugin", (compilation) => {
+          const outputPath = path.resolve(__dirname, "dist");
+          const outputFileName = "RS_MessageSystem.js";
+
+          const topOfCommentAndLicense = fs.readFileSync(
+            path.resolve(__dirname, "comments", "Comments.js"),
+            "utf-8"
+          );
+          const content = fs.readFileSync(
+            path.resolve(outputPath, outputFileName),
+            "utf-8"
+          );
+          const output = `${topOfCommentAndLicense}${content}`;
+          fs.writeFileSync(path.resolve(outputPath, outputFileName), output);
+        });
+      },
+    },
+  ],
+};
+
+module.exports = [target];
