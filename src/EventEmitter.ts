53d609d1e4aa11da54fc64a1badd9c3a62d2202a
biud436 <biud436@gmail.com>
2022-02-04 20:18:36 +0900

    added new type in EventEmitter class

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -1,38 +1,63 @@
+// 함수 타입
+type FunctionLike = Function;
+// 이벤트 키 타입
+type EventKey = string;
+
+/**
+ * 이벤트 리스터를 정의합니다.
+ */
+type VMEventListener = { [key: EventKey]: Function[] };
+
 /**
  * @class EventEmitter
  * @description
  * 이벤트를 전달하기 위한 클래스입니다.
  */
 export class EventEmitter {
-  constructor(private _listeners: { [key: string]: Function[] } = {}) {
-    this._listeners = {};
-  }
-
-  on(eventName: string, func: Function) {
-    if (!this._listeners[eventName]) {
-      this._listeners[eventName] = [];
+    constructor(private _listeners: VMEventListener = {}) {
+        this._listeners = {};
     }
 
-    if (!func) {
-      return;
+    /**
+     * 호출할 이벤트를 등록합니다.
+     *
+     * @param eventName
+     * @param func
+     * @returns
+     */
+    on(eventName: string, func: FunctionLike) {
+        if (!this._listeners[eventName]) {
+            this._listeners[eventName] = [];
+        }
+
+        if (!func) {
+            return;
+        }
+
+        if (func instanceof Function) {
+            this._listeners[eventName].push(func);
+        }
     }
 
-    if (func instanceof Function) {
-      this._listeners[eventName].push(func);
-    }
-  }
+    /**
+     * 등록된 이벤트를 호출합니다.
+     *
+     * @param eventName
+     * @param args
+     * @returns
+     */
+    emit(eventName: string, ...args: any[]) {
+        if (!this._listeners[eventName]) {
+            return;
+        }
 
-  emit(eventName: string, ...args: any[]) {
-    if (!this._listeners[eventName]) {
-      return;
-    }
+        this._listeners[eventName].forEach((func) => {
+            if (!(func instanceof Function)) {
+                console.warn("호출된 이벤트 리스너가 함수가 아닙니다.");
+                return;
+            }
 
-    this._listeners[eventName].forEach((func) => {
-      if (!(func instanceof Function)) {
-        console.warn("호출된 이벤트 리스너가 함수가 아닙니다.");
-        return;
-      }
-      func(...args);
-    });
-  }
+            func(...args);
+        });
+    }
 }
