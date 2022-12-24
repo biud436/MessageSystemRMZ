b4c116709ce2b8f2e22c9ec26a8264311d591569
biud436 <biud436@gmail.com>
2022-12-24 12:52:55 +0900

    chore: upgrade webpack 5 and added config.yml file

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,76 @@
 const path = require("path");
 const fs = require("fs");
 const webpack = require("webpack");
-// const { CheckerPlugin } = require("awesome-typescript-loader");
+const yaml = require("js-yaml");
+
+function loadConfig() {
+    try {
+        const contents = fs.readFileSync(
+            path.resolve(__dirname, "config.yml"),
+            "utf8"
+        );
+        const doc = yaml.load(contents);
+        return doc;
+    } catch (e) {
+        console.warn(e);
+    }
+}
 
 const target = {
-  mode: "production", // none' | 'development' | 'production'
-  entry: path.join(__dirname, "src", "index.ts"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: `RS_MessageSystem.js`,
-  },
-  target: "web",
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-        include: [path.resolve(__dirname, "src")],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".ts", ".json"],
-  },
-  plugins: [
-    // new CheckerPlugin(),
-    new webpack.DefinePlugin({
-      RS: "RS",
-    }),
-    {
-      apply: (compiler) => {
-        compiler.hooks.afterEmit.tap("AfterEmitPlugin", (compilation) => {
-          const outputPath = path.resolve(__dirname, "dist");
-          const outputFileName = "RS_MessageSystem.js";
-
-          const topOfCommentAndLicense = fs.readFileSync(
-            path.resolve(__dirname, "comments", "Comments.js"),
-            "utf-8"
-          );
-          const content = fs.readFileSync(
-            path.resolve(outputPath, outputFileName),
-            "utf-8"
-          );
-          const output = `${topOfCommentAndLicense}${content}`;
-          fs.writeFileSync(path.resolve(outputPath, outputFileName), output);
-        });
-      },
+    mode: "production", // none' | 'development' | 'production'
+    entry: path.join(__dirname, "src", "index.ts"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: `RS_MessageSystem.js`,
     },
-  ],
-  devtool: "source-map",
+    target: "web",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+                include: [path.resolve(__dirname, "src")],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".json"],
+    },
+    plugins: [
+        // new CheckerPlugin(),
+        new webpack.DefinePlugin({
+            RS: "RS",
+        }),
+        {
+            apply: (compiler) => {
+                compiler.hooks.afterEmit.tap(
+                    "AfterEmitPlugin",
+                    (compilation) => {
+                        const outputPath = path.resolve(__dirname, "dist");
+                        // const outputFileName = "RS_MessageSystem.js";
+                        const config = loadConfig();
+                        const outputFileName = config.outputs.name;
+
+                        const topOfCommentAndLicense = fs.readFileSync(
+                            path.resolve(__dirname, "comments", "Comments.js"),
+                            "utf-8"
+                        );
+                        const content = fs.readFileSync(
+                            path.resolve(outputPath, outputFileName),
+                            "utf-8"
+                        );
+                        const output = `${topOfCommentAndLicense}${content}`;
+                        fs.writeFileSync(
+                            path.resolve(outputPath, outputFileName),
+                            output
+                        );
+                    }
+                );
+            },
+        },
+    ],
+    devtool: "source-map",
 };
 
 module.exports = [target];
