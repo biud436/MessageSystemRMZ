78b2785df86bd072575cd0b557b98c5674925afd
biud436 <biud436@gmail.com>
2022-12-24 14:17:48 +0900

    refactor: Window_Base and Window_Message are splitted to commands folder

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { getBitmapCommand } from "./commands/Bitmap.command";
 import { getMainCommand } from "./commands/Main.command";
 import { getGameCommandCommand } from "./commands/GameMessage.command";
 import { getSpriteBattlerCommand } from "./commands/SpriteBattler.command";
+import { getWindowBaseCommand } from "./commands/WindowBase.command";
+import { getWindowMessageCommand } from "./commands/WindowMessage.command";
 
 const executor = ComponentExecutor.getInstance();
 
@@ -13,11 +15,15 @@ executor
     .add("bitmap", getBitmapCommand())
     .add("Game_Message", getGameCommandCommand())
     .add("Sprite_Battler", getSpriteBattlerCommand())
+    .add("Window_Base", getWindowBaseCommand())
+    .add("Window_Message", getWindowMessageCommand())
     .add("main", getMainCommand())
     // active commands
     .ready("bitmap")
     .ready("Game_Message")
     .ready("Sprite_Battler")
+    .ready("Window_Base")
+    .ready("Window_Message")
     .ready("main")
     // execute commands
     .executeAll();
