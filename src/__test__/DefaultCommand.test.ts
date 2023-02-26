ce2ee3be657a843b475ccb1c9c7005eded456da4
biud436 <biud436@gmail.com>
2023-02-26 20:50:49 +0900

    fix: 테스트 코드 비활성화

--- src/__test__/DefaultCommand.test.ts
+++ src/__test__/DefaultCommand.test.ts
@@ -2,7 +2,7 @@ import { Executuor } from "../core/ComponentExecutor";
 
 export function getDefaultCommandTest(): Executuor {
     return () => {
-        $gameMessage.clear();
-        $gameMessage.add("Hello, World!");
+        // $gameMessage.clear();
+        // $gameMessage.add("Hello, World!");
     };
 }
